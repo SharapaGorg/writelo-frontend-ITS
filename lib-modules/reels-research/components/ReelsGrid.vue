b54cc9c948adbdf965666522de6cb317d2183ce9
SharapaGorg <dushin.egor.dm@yandex.ru>
2026-03-25 13:36:56 +0300

    feat(reels-research): add ReelsGrid component

---
+++ lib-modules/reels-research/components/ReelsGrid.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useReelsResearchStore } from '../stores/reelsResearchStore'
+import ReelCard from './ReelCard.vue'
+
+const store = useReelsResearchStore()
+</script>
+
+<template>
+  <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+    <ReelCard
+      v-for="reel in store.filteredReels"
+      :key="reel.id"
+      :reel="reel"
+    />
+  </div>
+</template>
