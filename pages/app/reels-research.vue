b4b01a85117d8cbdc3d764378cbe1034ea46382e
SharapaGorg <dushin.egor.dm@yandex.ru>
2026-03-25 13:38:14 +0300

    feat(reels-research): add page route

---
+++ pages/app/reels-research.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ReelsResearchPage } from '~/lib-modules/reels-research'
+
+definePageMeta({
+  layout: 'app'
+})
+</script>
+
+<template>
+  <ReelsResearchPage />
+</template>
